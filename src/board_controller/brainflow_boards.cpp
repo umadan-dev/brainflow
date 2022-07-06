d5da9a07cb7f9f3d7ee252cdcbce6e73cb59cfe1
madan-ujjwal <ujjwal.madan@capillarytechcom>
2022-07-06 14:32:09 +0530

    added neuphony in brainflow boards

--- src/board_controller/brainflow_boards.cpp
+++ src/board_controller/brainflow_boards.cpp
@@ -566,6 +566,17 @@ json brainflow_boards_json = {
             {"eeg_channels", {1, 2, 3, 4, 5, 6, 7, 8}},
             {"emg_channels", {1, 2, 3, 4, 5, 6, 7, 8}},
             {"ecg_channels", {1, 2, 3, 4, 5, 6, 7, 8}}
+        }},
+        {"44",
+            {{"name", "Neuphony"},
+            {"sampling_rate", 250},
+            {"package_num_channel", 0},
+            {"num_rows", 12},
+            {"eeg_channels", {1, 2, 3, 4, 5, 6, 7, 8}},
+            {"eeg_names", "Fp1,Fp2,F3,F4,T3,T4,Fz,Pz"},
+            {"accel_channels", {9, 10, 11}},
+            {"other_channels", {12}}
+
         }}
     }
 }};
